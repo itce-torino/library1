1bf657bde81ea0c839b53d76bbdb9d7ec95aa513
Cesare Esposito <cesare.esposito@email.it>
2021-01-03 16:50:39 +0100

    showToast with runnable function SUM-Ready to test

--- app/src/main/java/itce/library/host/MainActivity.kt
+++ app/src/main/java/itce/library/host/MainActivity.kt
@@ -7,23 +7,27 @@ import itce.library.library1.ItceUtility
 import itce.library.library1.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var positiveFunction: Runnable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        positiveFunction = thisDisplay()
 
         ItceUtility.showToast(
             this,
             "Execution of 'ItceUtility.sum(16, 20)' (on Yes)",
             "Yes",
-            //  funPos = thisDisplay(),
+            positiveFunction,
             "NO",
             "Cancel",
             true
         )
     }
 
-    private fun thisDisplay() {
+    private fun thisDisplay() = Runnable() {
         ItceHelper.justToast(
             this,
             ItceUtility.sum(16, 20).toString(),
